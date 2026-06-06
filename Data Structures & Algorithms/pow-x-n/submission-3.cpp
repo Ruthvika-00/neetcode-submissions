0c7a80567a5cb1e2b54e23ab18155b2efa75bc60
Ruthvika-00 <ruthvikamartha@gmail.com>
2026-06-06 19:39:30 +0530

    Add: pow-x-n - submission-3

---
+++ Data Structures & Algorithms/pow-x-n/submission-3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double mypow(double x, int n){
+        if(n==0) return 1;
+        double half = mypow(x,n/2);
+        
+        return (n%2==0)? half*half : x*half*half;
+    }
+    double myPow(double x, int n) {
+        if(x==0) return 0;
+        if(n==0) return 1;
+        
+        double res = mypow(x,abs(static_cast<long>(n)));
+        return (n>=0) ? res : 1/res;
+    }
+};
