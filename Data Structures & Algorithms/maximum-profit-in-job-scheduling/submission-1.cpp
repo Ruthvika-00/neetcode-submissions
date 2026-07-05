babb29b2cd595b69cd60f56b2ee0eaa60ac6abe1
Ruthvika-00 <ruthvikamartha@gmail.com>
2026-07-05 22:50:50 +0530

    Add: maximum-profit-in-job-scheduling - submission-1

---
+++ Data Structures & Algorithms/maximum-profit-in-job-scheduling/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        int n=startTime.size();
+        vector<vector<int>> jobs;
+        for(int i=0;i<n;i++){
+            jobs.push_back({startTime[i],endTime[i],profit[i]});
+        }
+        sort(jobs.begin(),jobs.end());
+        vector<int> starts;
+        for(auto &j : jobs){
+            starts.push_back(j[0]);
+        }
+        vector<int> dp(n+1,0);
+        for(int i=n-1;i>=0;i--){
+            int nxt= lower_bound(
+                starts.begin(),
+                starts.end(),
+                jobs[i][1]
+            )-starts.begin();
+            int take=jobs[i][2]+dp[nxt];
+            int skip=dp[i+1];
+            dp[i]=max(take,skip);
+        }
+        return dp[0];
+
+    }
+};
